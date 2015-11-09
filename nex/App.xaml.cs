0e0c4508f0ccb7db316560f09c60a5be95eec3fb
EbXpJ6bp <csoft@outlook.jp>
2015-11-09 21:49:27 +0900

    Update App.xaml.cs "Application_Startup" add

--- nex/App.xaml.cs
+++ nex/App.xaml.cs
@@ -8,10 +8,23 @@ using System.Windows;
 
 namespace nex
 {
-    /// <summary>
-    /// App.xaml の相互作用ロジック
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+	/// <summary>
+	/// App.xaml の相互作用ロジック
+	/// </summary>
+	public partial class App : Application
+	{
+		private splash _splash;
+		public splash Splash { get; private set; }
+
+		public App() : base()
+		{
+			Startup += Application_Startup;
+		}
+
+		private void Application_Startup(object sender, StartupEventArgs e)
+		{
+			Splash = new splash();
+			Splash.Show();
+		}
+	}
 }
